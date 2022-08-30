ddc33e1178a31eced822b30e55b496a81578cd67
Patrick Koers <pkoers75@gmail.com>
2022-08-30 16:28:09 +0200

    profiles controller is back

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,12 @@
 class ProfilesController < ApplicationController
+  before_action :set_user, only: :show
 
   def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -59,6 +59,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_133747) do
     t.index ["user_id"], name: "index_collections_on_user_id"
   end
 
+  create_table "user_profiles", force: :cascade do |t|
+    t.string "username"
+    t.text "bio"
+    t.string "answers", default: [], array: true
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,9 +77,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_133747) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
-    t.text "bio"
-    t.string "answers", default: [], array: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -92,4 +99,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_133747) do
   add_foreign_key "collection_wines", "collections"
   add_foreign_key "collection_wines", "wines"
   add_foreign_key "collections", "users"
+  add_foreign_key "user_profiles", "users"
 end
