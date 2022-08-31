1777e60ce7b21b27bc32954f77032555b7aa02d2
joao garcia <jiboiadamor@gmail.com>
2022-08-31 16:03:39 +0100

    First front end design for Results

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -6,10 +6,18 @@ class QuizzesController < ApplicationController
 
   def result
     @answers = session[:answers]
+    recommend_wine
   end
 
   def create
     session[:answers] = params[:quizz].values
     redirect_to result_quizzes_path
+  end
+
+  def recommend_wine
+    # for now the recommended wines are randomly chosen
+    @wines = Wine.all
+    @recommend_one = Wine.order('RANDOM()').first
+    @recommend_two = Wine.order('RANDOM()').last
   end
 end
