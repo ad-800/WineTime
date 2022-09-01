d9831bd32ef32e39741eab996362a332a3ed8b3b
joao garcia <jiboiadamor@gmail.com>
2022-09-01 12:03:50 +0100

    Search bar working

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,4 +1,12 @@
 class Wine < ApplicationRecord
   has_one_attached :photo
   has_many :collection_wines
+
+  include PgSearch::Model
+
+  pg_search_scope :search_wines,
+    against: [ :name, :color, :country, :region ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
