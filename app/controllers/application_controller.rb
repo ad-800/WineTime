c32231c70ae0f486f0ffde7fedd273acb1fc0d22
Michele Scotellaro <scotellaro.michele@gmail.com>
2022-09-05 17:32:43 +0100

    display wines by personality type

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[answers username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[answers username personality_id])
 
     devise_parameter_sanitizer.permit(:account_update, keys: %i[photo username bio])
   end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,8 +15,9 @@ class ProfilesController < ApplicationController
 
   def recommend_wine
     # for now the recommended wines are randomly chosen
-    @wines = Wine.all
-    @recommended = Wine.order('RANDOM()').sample(3)
+    # @wines = Wine.all
+    # @recommended = Wine.order('RANDOM()').sample(3)
+    @recommended = wines_by_personality_type
   end
 
   private
@@ -24,5 +25,71 @@ class ProfilesController < ApplicationController
   def set_user
     # @user = User.find(params[:id])
     @user = User.find_by(username: params[:id])
+  end
+
+  def wines_by_personality_type
+    # get the personality type
+    personality_type = current_user.personality.personality_type
+
+    case personality_type
+      when 1 # Stoic
+        wines = Wine.where(
+          bitter: 5..9,
+          fruit: 5..9,
+          mineral: 5..9,
+          spice: 2..5,
+          earth: 2..5
+        )
+      when 2 # Kickstarter
+        wines = Wine.where(
+          bitter: 7..10,
+          fruit: 6..10,
+          mineral: 6..10,
+          spice: 6..10,
+          earth: 3..6
+        )
+      when 3 # Connoisseur
+        wines = Wine.where(
+          bitter: 6..9,
+          fruit: 1..3,
+          mineral: 3..5,
+          spice: 3..5,
+          earth: 5..9
+        )
+      when 4 # Mediator
+        wines = Wine.where(
+          bitter: 3..6,
+          fruit: 6..10,
+          mineral: 5..8,
+          spice: 6..10,
+          earth: 1..3
+        )
+      when 5 # Romantic
+        wines = Wine.where(
+          bitter: 4..7,
+          fruit: 4..7,
+          mineral: 4..7,
+          spice: 4..7,
+          earth: 4..7
+        )
+      when 6 # Conservator
+        wines = Wine.where(
+          bitter: 7..10,
+          fruit: 1..3,
+          mineral: 3..7,
+          spice: 1..3,
+          earth: 1..5
+        )
+      when 7 # Adventurer
+        wines = Wine.where(
+          bitter: 1..10,
+          fruit: 1..10,
+          mineral: 1..10,
+          spice: 1..10,
+          earth: 1..10)
+    end
+
+    # get wines by personality type
+    wines
   end
 end
